e28699d937294835243c906b1a9a15906de95275
artem <yaroslav.gorbatyuk.2010@gmail.com>
2024-10-13 15:18:26 +0300

    as

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class HelloController {
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping()
+    public String hello() {
+        return "hello";
+    }
+}
